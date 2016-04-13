4639b012437b0ddbefd5af166e904b727d30fc43
Dharmesh Gohil <dgohil@groupon.com>
2016-04-13 09:13:04 -0700

    additional functionality support

--- app/src/main/java/com/vistrav/poplib/MainActivity.java
+++ app/src/main/java/com/vistrav/poplib/MainActivity.java
@@ -3,12 +3,16 @@ package com.vistrav.poplib;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.vistrav.pop.Pop;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,20 +40,31 @@ public class MainActivity extends AppCompatActivity {
         } else if (view.getId() == R.id.button_cust_layout) {
             Pop.on(this)
                     .with()
+                    .icon(R.mipmap.ic_launcher)
+                    .cancelable(false)
                     .title(R.string.title)
                     .layout(R.layout.custom_pop)
                     .when(new Pop.Yah() {
                         @Override
                         public void clicked(DialogInterface dialog, View view) {
-                            Toast.makeText(getBaseContext(), "Yah button clicked", Toast.LENGTH_LONG).show();
+                            EditText etName = (EditText) view.findViewById(R.id.et_name);
+                            Toast.makeText(getBaseContext(), "Name :: " + etName.getText(), Toast.LENGTH_LONG).show();
                         }
                     })
-                    .when(new Pop.Nah() {
+                    .when(R.string.nah_button, new Pop.Nah() {
                         @Override
                         public void clicked(DialogInterface dialog, View view) {
                             Toast.makeText(getBaseContext(), "Nah button clicked", Toast.LENGTH_LONG).show();
                         }
-                    }).show();
+                    })
+                    .show(new Pop.View() {
+                        @Override
+                        public void prepare(View view) { // assign value to view element
+                            EditText etName = (EditText) view.findViewById(R.id.et_name);
+                            Log.i(TAG, "etName :: " + etName.getText());
+                            etName.setText("Test Name 123");
+                        }
+                    });
         } else if (view.getId() == R.id.button_no_button) {
             Pop.on(this).with().title(R.string.title).body(R.string.body).show();
         }
--- pop/src/main/java/com/vistrav/pop/Pop.java
+++ pop/src/main/java/com/vistrav/pop/Pop.java
@@ -2,19 +2,17 @@ package com.vistrav.pop;
 
 import android.app.Activity;
 import android.content.DialogInterface;
+import android.graphics.drawable.Drawable;
+import android.support.annotation.DrawableRes;
 import android.support.annotation.LayoutRes;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.annotation.StringRes;
 import android.support.v7.app.AlertDialog;
-import android.view.LayoutInflater;
 
+@SuppressWarnings("unused")
 public class Pop {
 
-    @StringRes
-    private int title;
-    @StringRes
-    private int body;
-    @LayoutRes
-    private int layout;
     @StringRes
     private int yahButtonName;
     @StringRes
@@ -27,9 +25,12 @@ public class Pop {
     private Nah nah;
     private Aah aah;
     private Pop.View popView;
+    private AlertDialog.Builder builder;
+    private android.view.View lView;
 
     protected Pop(Activity activity) {
         this.activity = activity;
+        builder = new AlertDialog.Builder(activity);
     }
 
     public static Pop on(Activity activity) {
@@ -41,45 +42,58 @@ public class Pop {
     }
 
     public Pop title(@StringRes int title) {
-        this.title = title;
+        builder.setTitle(title);
+        return this;
+    }
+
+    public Pop title(@NonNull android.view.View view) {
+        builder.setCustomTitle(view);
+        return this;
+    }
+
+    public Pop icon(@NonNull Drawable icon) {
+        builder.setIcon(icon);
+        return this;
+    }
+
+    public Pop cancelable(boolean cancelable) {
+        builder.setCancelable(cancelable);
+        return this;
+    }
+
+    public Pop icon(@DrawableRes int icon) {
+        builder.setIcon(icon);
+        return this;
+    }
+
+    public Pop title(@NonNull String title) {
+        builder.setTitle(title);
         return this;
     }
 
     public Pop body(@StringRes int body) {
-        this.body = body;
+        builder.setMessage(body);
+        return this;
+    }
+
+
+    public Pop body(@NonNull String body) {
+        builder.setMessage(body);
         return this;
     }
 
     public Pop layout(@LayoutRes int layout) {
-        this.layout = layout;
+        lView = activity.getLayoutInflater().inflate(layout, null);
+        builder.setView(lView);
         return this;
     }
 
     private void createDialog() {
-
-        AlertDialog.Builder builder = new AlertDialog.Builder(activity);
-        if (title != 0) {
-            builder.setTitle(title);
-        }
-        final android.view.View view;
-        if (body != 0) {
-            builder.setMessage(body);
-        }
-        if (layout != 0) {
-            LayoutInflater inflater = activity.getLayoutInflater();
-            view = inflater.inflate(layout, null);
-            builder.setView(view);
-            if (popView != null) {
-                popView.prepare(view);
-            }
-        } else {
-            view = null;
-        }
         if (yah != null) {
             int buttonId = yahButtonName != 0 ? yahButtonName : R.string.ok;
             builder.setPositiveButton(buttonId, new DialogInterface.OnClickListener() {
                 public void onClick(DialogInterface dialog, int id) {
-                    yah.clicked(dialog, view);
+                    yah.clicked(dialog, lView);
                 }
             });
         }
@@ -87,7 +101,7 @@ public class Pop {
             int buttonId = nahButtonName != 0 ? nahButtonName : R.string.cancel;
             builder.setNegativeButton(buttonId, new DialogInterface.OnClickListener() {
                 public void onClick(DialogInterface dialog, int id) {
-                    yah.clicked(dialog, view);
+                    nah.clicked(dialog, lView);
                 }
             });
         }
@@ -95,7 +109,7 @@ public class Pop {
             int buttonId = aahButtonName != 0 ? aahButtonName : R.string.neutral;
             builder.setNegativeButton(buttonId, new DialogInterface.OnClickListener() {
                 public void onClick(DialogInterface dialog, int id) {
-                    aah.clicked(dialog, view);
+                    aah.clicked(dialog, lView);
                 }
             });
         }
@@ -109,47 +123,47 @@ public class Pop {
         return alertDialog;
     }
 
-    public AlertDialog show(Pop.View popView) {
-        this.popView = popView;
+    public AlertDialog show(@NonNull Pop.View popView) {
+        popView.prepare(lView);
         createDialog();
         return alertDialog;
     }
 
-    public Pop when(Yah yah) {
+    public Pop when(@NonNull Yah yah) {
         this.yah = yah;
         return this;
     }
 
-    public Pop when(Aah aah) {
+    public Pop when(@NonNull Aah aah) {
         this.aah = aah;
         return this;
     }
 
-    public Pop when(Nah nah) {
-        this.nah = nah != null ? nah : new NahImpl();
+    public Pop when(@NonNull Nah nah) {
+        this.nah = nah;
         return this;
     }
 
-    public Pop when(int yahButtonName, Yah yah) {
+    public Pop when(@StringRes int yahButtonName, @NonNull Yah yah) {
         this.yah = yah;
         this.yahButtonName = yahButtonName;
         return this;
     }
 
-    public Pop when(int aahButtonName, Aah aah) {
+    public Pop when(@StringRes int aahButtonName, @NonNull Aah aah) {
         this.aah = aah;
         this.aahButtonName = aahButtonName;
         return this;
     }
 
-    public Pop when(int nahButtonName, Nah nah) {
+    public Pop when(@StringRes int nahButtonName, @NonNull Nah nah) {
         this.nah = nah;
         this.nahButtonName = nahButtonName;
         return this;
     }
 
-    public interface Clickable {
-        void clicked(DialogInterface dialog, android.view.View view);
+    private interface Clickable {
+        void clicked(DialogInterface dialog, @Nullable android.view.View view);
     }
 
     public interface Yah extends Clickable {
@@ -163,12 +177,12 @@ public class Pop {
 
 
     public interface View {
-        void prepare(android.view.View view);
+        void prepare(@Nullable android.view.View view);
     }
 
     public class NahImpl implements Nah {
         @Override
-        public void clicked(DialogInterface dialog, android.view.View view) {
+        public void clicked(DialogInterface dialog, @Nullable android.view.View view) {
             if (dialog != null) {
                 dialog.dismiss();
             }
